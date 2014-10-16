c2972252b652a2b5bf4b67d0d66007bfb42bd7ee
bobbear <bo6bear@gmail.com>
2014-10-16 22:46:35 +0800

    add cart module and validate

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    calculate_access_index_times_for_one_user
+  end
+  private
+  def calculate_access_index_times_for_one_user 
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
   #默认使用title进行排序
   #default_scope {where(order:'title')}
 
+  has_many :line_items
+
   validates :title, :description, :image_url, :presence => true
 
   validates :title, :uniqueness => true, :length => {:minimum => 10}
@@ -13,4 +15,17 @@ class Product < ActiveRecord::Base
     :with      => %r{\.(gif|jpg|png)\z}i,
     :message   => 'must be a URL for GIF, JPG or PNG images.'
   }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present还有定单存在')
+      return false
+    end
+  end
 end
